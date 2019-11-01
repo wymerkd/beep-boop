5f9617d8d6b4f1aa474d93a2e8cf703ee0ce6a98
wymerkd <wymerkd@gmail.com>
2019-11-01 16:53:44 -0700

    fix indentation and remove unnecessary comments

--- js/scripts.js
+++ js/scripts.js
@@ -18,23 +18,11 @@ var counter = function(numbersCounted) {
 
 var identify = function(number) {
   for (i=0; i <= userConvertedNumbers; i++) {
-  if (i === "1") {
-    userNumbers.push(beep);
+    if (i === "1") {
+      userNumbers.push(beep);
+    }
+  }
 }
-}
-}
-
-//   } else if (i === 2) {
-//     userNumbers.push(boop);
-//   } else if (i === 3) {
-//     userNumbers.push(sorry);
-//   } else {
-//     userNumbers.push(i);
-//   }
-// }
-// }
-
-
 
 //User Interface Logic
 $(document).ready(function() {
@@ -47,106 +35,3 @@ $(document).ready(function() {
     userNumbers2 = [];
   });
 });
-
-
-
-
-
-
-// userNumbers.forEach(function(result) {
-  //   if (result === 1) {
-    //     newUserResult.push(beep);
-    //   }
-    //
-    // });
-
-
-
-
-    // function identifier(userNumbers) {
-      //   for (i=0; i <= userNumbers; i++) {
-        //     if (i === 1) {
-          //       newUserResult.push(beep);
-          //     } else if (i === 2) {
-            //       newUserResult.push(boop);
-            //     } else if (i === 3) {
-              //       newUserResult.push(sorry);
-              //     } else {
-                //       newUserResult.push(i);
-                //     }
-                //   }
-                // }
-
-
-
-
-
-
-
-
-
-                //   } for (i=0; i <= numbersCounted; i++)
-                //   if (i === 1) {
-                  //     userNumbers.push(beep);
-                  //   } else if (i === 2) {
-                    //     userNumbers.push(boop);
-                    //   } else if (i === 3) {
-                      //     userNumbers.push(sorry);
-                      //   } else {
-                        //     userNumbers.push(i);
-                        //   }
-                        // }
-                        // }
-
-
-
-
-
-                        // var hey = tests.map(function(test) {
-                          //     if (numberAlerts.includes(test)){
-                            //       newUserResult.push(beep);
-                            //     // } else if (i === 2) {
-                              //     //   userNumbers.push(boop);
-                              //     // } else if (i === 3) {
-                                //     //   userNumbers.push(sorry);
-                                //     } else {
-                                  //       userNumbers.push(i);
-                                  //     }
-                                  //   });
-
-
-
-
-
-                                  // for loop to see if test string includes 1 2 3
-
-
-                                  // var splitNumbers = function(test) {
-                                    //   userNumbers.toString().split('').push();
-                                    // }
-
-
-
-                                    // var test = userNumbers.map(function(result) {
-                                      //       if (one.includes(result)){
-                                        //         return(beep)
-                                        //       } else {
-                                          //         return (userNumbers)
-                                          //       }
-                                          //     });
-
-
-                                          //     if (i === 1) {
-                                            //       userNumbers.push(beep);
-                                            //     } else if (i === 2) {
-                                              //       userNumbers.push(boop);
-                                              //     } else if (i === 3) {
-                                                //       userNumbers.push(sorry);
-                                                //     } else {
-                                                  //       userNumbers.push(i);
-                                                  //     }
-                                                  //   }
-                                                  // }
-                                                  //
-
-                                                  // userNumbers.push(i);
