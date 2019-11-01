bd5429c6eb840ab403238415611bdf279daca29b
wymerkd <wymerkd@gmail.com>
2019-11-01 14:29:28 -0700

    remove toString function and map

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,15 @@
 
+
 //Business Logic
 
 var beep = "Beep!"
 var boop = "Boop!"
 var sorry = "I'm sorry, Dave. I'm afraid I can't do that."
+// var numberAlerts = ["1", "2", "3"]
+
 var userResult = [];
-var test = userResult.toString();
+// var newUserResult = [];
+// var tests = userResult.toString();
 
 var counter = function(numbersCounted) {
   for (i=0; i <= numbersCounted; i++) {
@@ -13,48 +17,69 @@ var counter = function(numbersCounted) {
   }
 }
 
+// var hey = tests.map(function(test) {
+//     if (numberAlerts.includes(test)){
+//       newUserResult.push(beep);
+//     // } else if (i === 2) {
+//     //   userResult.push(boop);
+//     // } else if (i === 3) {
+//     //   userResult.push(sorry);
+//     } else {
+//       userResult.push(i);
+//     }
+//   });
+
+
+
+
+
+// for loop to see if test string includes 1 2 3
+
+
 // var splitNumbers = function(test) {
-//   userResult.toString().split('').push();
-// }
+  //   userResult.toString().split('').push();
+  // }
 
 
 
-// var test = userResult.map(function(result) {
-//       if (one.includes(result)){
-//         return(beep)
-//       } else {
-//         return (userResult)
-//       }
-//     });
+  // var test = userResult.map(function(result) {
+    //       if (one.includes(result)){
+      //         return(beep)
+      //       } else {
+        //         return (userResult)
+        //       }
+        //     });
 
-
-    // if (i === 1) {
-    //   userResult.push(beep);
-    // } else if (i === 2) {
-    //   userResult.push(boop);
-    // } else if (i === 3) {
-    //   userResult.push(sorry);
-    // } else {
-    //   userResult.push(i);
+    //
+    //     if (i === 1) {
+    //       userResult.push(beep);
+    //     } else if (i === 2) {
+    //       userResult.push(boop);
+    //     } else if (i === 3) {
+    //       userResult.push(sorry);
+    //     } else {
+    //       userResult.push(i);
+    //     }
+    //   }
     // }
-    // }
-    // }
+    //
 
-
-// userResult.push(i);
+    // userResult.push(i);
 
 
 
 
-//User Interface Logic
-$(document).ready(function() {
-  $("form#submitForm").submit(function(event) {
-    event.preventDefault();
-    var userInput = parseInt($("input#userInput").val());
-    var numbersCounted = counter(userInput);
-    var test = userResult.toString().split('');
-    console.log(test)
-    $("#displayResult").text(test);
-    test = [];
-  });
-});
+    //User Interface Logic
+    $(document).ready(function() {
+      $("form#submitForm").submit(function(event) {
+        event.preventDefault();
+        var userInput = parseInt($("input#userInput").val());
+        var numbersCounted = counter(userInput);
+        // var tests = userResult.toString().split('');
+
+        // var numbersCounted2 = counter2(test);
+          // console.log(tests)
+          $("#displayResult").text(userResult);
+          userResult = [];
+        });
+      });
