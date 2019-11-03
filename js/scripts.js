5c570b10be37d719de8116818a8d56cec0310b4e
wymerkd <wymerkd@gmail.com>
2019-11-02 20:44:57 -0700

    change order of if/else if conditions to match application objectives.

--- js/scripts.js
+++ js/scripts.js
@@ -7,32 +7,30 @@ var one = "1"
 var two = "2"
 var three ="3"
 
-
+// var finalResults = newArray
 var userNumbers = [];
 
 function counter(countedNumbers) {
   for (i=0; i <= countedNumbers; i++) {
-  userNumbers.push(i.toString());
+    userNumbers.push(i.toString());
   }
 }
 
 
-
 function convert() {
-var newArray = userNumbers.map(function(userNumber) {
-     if (userNumber.includes(one)){
-       return(beep)
-     } else if (userNumber.includes(two)) {
-       return (boop)
-     } else if (userNumber.includes(three)) {
-       return (sorry)
-     } else {
-       return (userNumber)
-     }
-
-   });
-   console.log(newArray);
- }
+  var newArray = userNumbers.map(function(userNumber) {
+    if (userNumber.includes(three)){
+      return(sorry)
+    } else if (userNumber.includes(two)) {
+      return (boop)
+    } else if (userNumber.includes(one)) {
+      return (beep)
+    } else {
+      return (userNumber)
+    }
+  });
+  console.log(newArray);
+}
 
 
 //User Interface Logic
@@ -41,12 +39,11 @@ $(document).ready(function() {
     event.preventDefault();
     var userInput = parseInt($("input#userInput").val());
     var countedNumbers = counter(userInput);
+    var userResults = convert();
 
-    convert();
 
     console.log(userNumbers);
 
-
-    $("#displayResult").text(userNumbers);
+    // $("#displayResult").text(finalResults);
   });
 });
