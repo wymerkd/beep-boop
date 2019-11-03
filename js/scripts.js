c0751a0e2a6ae8b75e59a2de50320caeeee507fd
wymerkd <wymerkd@gmail.com>
2019-11-02 18:58:44 -0700

    create map loop that checks if userNumbers array includes 1 or 2, and returns beep or boop if true.

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,8 @@ var beep = "Beep!"
 var boop = "Boop!"
 var sorry = "I'm sorry, Dave. I'm afraid I can't do that."
 var one = "1"
+var two = "2"
+var three ="3"
 
 
 var userNumbers = [];
@@ -18,11 +20,37 @@ var convertedNumbers = [];
 function counter(countedNumbers) {
   for (i=0; i <= countedNumbers; i++) {
     userNumbers.push(i.toString());
-    if (one.includes(userNumbers[i])) {
-    return "beep";
   }
 }
-}
+
+function convert() {
+var newArray = userNumbers.map(function(userNumber) {
+     if (one.includes(userNumber)){
+       return(beep)
+     } else if (two.includes(userNumber)) {
+       return (boop)
+     } else {
+       return (userNumber)
+     }
+
+   });
+   console.log(newArray);
+ }
+
+
+
+
+
+//     if (three.includes(userNumbers[i])) {
+//     return sorry;
+//   } else if (two.includes(userNumbers[i])) {
+//     return boop;
+//   // } else if (three.includes(userNumbers[i])) {
+//   //   return sorry;
+//
+// }
+// }
+// }
 
 
 
@@ -52,24 +80,20 @@ function counter(countedNumbers) {
 //   }
 
 
-
-
-
-
-
-
-
-
 //User Interface Logic
 $(document).ready(function() {
   $("form#submitForm").submit(function(event) {
     event.preventDefault();
     var userInput = parseInt($("input#userInput").val());
     var countedNumbers = counter(userInput);
+    convert();
+
     // var convertNumbers = convert(countedNumbers);
     // var result = checkFunction(convertedNumbers);
-    console.log(countedNumbers);
 
-    $("#displayResult").text(convertedNumbers);
+    console.log(userNumbers);
+
+
+    $("#displayResult").text(userNumbers);
   });
 });
